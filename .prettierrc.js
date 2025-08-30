6e0d4933cf6a986215290dde75f3a837fc113af0
Jimmy Thompson <jithompson@caesars.com>
2025-08-30 13:08:15 +0100

    Add prettier

---
+++ .prettierrc.js
@@ -0,0 +1,11 @@
+module.exports = {
+  arrowParens: 'avoid',
+  bracketSpacing: true,
+  jsxBracketSameLine: true,
+  printWidth: 120,
+  semi: true,
+  singleQuote: true,
+  tabWidth: 2,
+  trailingComma: 'none',
+  useTabs: false
+};
--- src/js/main.js
+++ src/js/main.js
@@ -9,38 +9,36 @@ const defaultOptions = {
   itemsSelector: '.d-carousel__item',
   nextSelector: '.d-carousel__next',
   prevSelector: '.d-carousel__prev',
-  easing: [0.42,0,0.58,1], // ease-in-out, used for animating the scroll. See https://github.com/gre/bezier-easing for options
-  delta: () => { // will count an item as fully in view if within ${x}px of delta
+  easing: [0.42, 0, 0.58, 1], // ease-in-out, used for animating the scroll. See https://github.com/gre/bezier-easing for options
+  delta: () => {
+    // will count an item as fully in view if within ${x}px of delta
     return 8;
   },
-  duration: () => { // possibly needs to be worked out for the amount that is being scrolled. E.g 100px will always scroll in 0.1s
+  duration: () => {
+    // possibly needs to be worked out for the amount that is being scrolled. E.g 100px will always scroll in 0.1s
     return 500;
-  },
+  }
 };
 
 const HALF_A_FRAME = 8; // Around half of a frame, based on 60fps as most common frame rate
-
 
 const throttler = rafThrottle();
 const windowResizeFns = [];
 const windowResize = () => {
   throttler(() => {
-    windowResizeFns.forEach((fn) => fn());
+    windowResizeFns.forEach(fn => fn());
   });
-}
+};
 
 let init = false;
 
-/** 
- * Times dCarousel was used to initalise a carousel. Used inregards to 
+/**
+ * Times dCarousel was used to initalise a carousel. Used inregards to
  * the window resize event listener. Which we only want one running at a time
  */
 let timesInit = 0;
 
-
-
 export default function dCarousel(el, optionsArg) {
-
   const eventHandlerOptions = supportsPassive ? { passive: true } : false;
 
   const options = Object.assign({}, defaultOptions, optionsArg);
@@ -53,24 +51,29 @@ export default function dCarousel(el, optionsArg) {
   const easing = BezierEasing.apply(null, options.easing);
 
   let containerWidth = el.offsetWidth;
-  let paddingLeft = options.paddingLeft ? parseFloat(window.getComputedStyle($outer).paddingLeft.replace('px', ''), 10) : 0;
+  let paddingLeft = options.paddingLeft
+    ? parseFloat(window.getComputedStyle($outer).paddingLeft.replace('px', ''), 10)
+    : 0;
   let innerWidth = $inner.offsetWidth;
   let itemWidth = $items[0].offsetWidth;
   let maxScrollLeft = innerWidth - containerWidth + paddingLeft;
   let scrollLeft;
   let animating = false;
 
-  const getFirstItemShowing = () => { // based off an index of 1
+  const getFirstItemShowing = () => {
+    // based off an index of 1
     const start = 0 - paddingLeft;
     return Math.ceil((start + scrollLeft - options.delta()) / itemWidth) + 1;
   };
 
-  const getLastItemShowing = () => { // based off an index of 1
+  const getLastItemShowing = () => {
+    // based off an index of 1
     const start = 0 - paddingLeft;
     return Math.floor((start + scrollLeft + containerWidth + options.delta()) / itemWidth);
   };
 
-  const getItemsShowing = () => { // returns the indexes of the items showing in an array. E.g only first item showing would return [0]
+  const getItemsShowing = () => {
+    // returns the indexes of the items showing in an array. E.g only first item showing would return [0]
     const first = getFirstItemShowing() - 1;
     const last = getLastItemShowing() - 1;
     const array = [];
@@ -80,13 +83,12 @@ export default function dCarousel(el, optionsArg) {
     return array;
   };
 
-  
   const outerScroll = () => {
     scrollLeft = $outer.scrollLeft;
 
     const customEvent = new CustomEvent('dCarousel:scroll', {
       detail: {
-        scrollLeft,
+        scrollLeft
       }
     });
 
@@ -96,8 +98,8 @@ export default function dCarousel(el, optionsArg) {
     } else if (scrollLeft === 0) {
       $prev.classList.add('disabled');
     }
-    
-    // minus 1 as if scrollLeft is a decimal then it may not be possible to fully match the maxScrollLeft. 
+
+    // minus 1 as if scrollLeft is a decimal then it may not be possible to fully match the maxScrollLeft.
     // So instead we will aim to be within 1px of the maxScrollLeft
     if (scrollLeft < maxScrollLeft - 1) {
       $next.classList.remove('disabled');
@@ -111,21 +113,20 @@ export default function dCarousel(el, optionsArg) {
     throttler(outerScroll);
   };
 
-  const scrollOuter = (x) => {
-
+  const scrollOuter = x => {
     animating = true;
     const diff = scrollLeft - x;
 
     const duration = options.duration({
       itemWidth,
       containerWidth,
-      diff: Math.abs(diff),
+      diff: Math.abs(diff)
     });
-    
+
     const startingScrollLeft = scrollLeft;
     let start = false;
-    const tick = (timestamp) => {
-      const lastTick = start === false ? false : ((timestamp - start) + HALF_A_FRAME) >= duration;
+    const tick = timestamp => {
+      const lastTick = start === false ? false : timestamp - start + HALF_A_FRAME >= duration;
       if (start === false) {
         start = timestamp;
       } else {
@@ -137,26 +138,27 @@ export default function dCarousel(el, optionsArg) {
           const percentage = easing(msDiff / duration); // the percentage of x that should to be scrolled to
           const xValue = diff * percentage; // x value that scrollLeft should be
 
-          if (diff < 0) { // going to the right
+          if (diff < 0) {
+            // going to the right
             $outer.scrollLeft = startingScrollLeft + Math.abs(xValue);
-          } else { // going to the left
+          } else {
+            // going to the left
             $outer.scrollLeft = startingScrollLeft - xValue;
           }
-          
         }
-        
       }
-      
+
       if (lastTick === false) requestAnimationFrame(tick);
     };
 
-    requestAnimationFrame(tick); 
-
+    requestAnimationFrame(tick);
   };
 
   const windowResizeFunction = () => {
     containerWidth = el.offsetWidth;
-    paddingLeft = options.paddingLeft ? parseFloat(window.getComputedStyle($outer).paddingLeft.replace('px', ''), 10) : 0;
+    paddingLeft = options.paddingLeft
+      ? parseFloat(window.getComputedStyle($outer).paddingLeft.replace('px', ''), 10)
+      : 0;
     innerWidth = $inner.offsetWidth;
     itemWidth = $items[0].offsetWidth;
     maxScrollLeft = innerWidth - containerWidth + paddingLeft;
@@ -172,7 +174,7 @@ export default function dCarousel(el, optionsArg) {
       return;
     }
     const nextItem = lastIndexFullyShowing + 1;
-    const x = Math.abs(start) + Math.min((nextItem * itemWidth) - itemWidth, innerWidth - containerWidth);
+    const x = Math.abs(start) + Math.min(nextItem * itemWidth - itemWidth, innerWidth - containerWidth);
     scrollOuter(x);
   };
 
@@ -185,7 +187,7 @@ export default function dCarousel(el, optionsArg) {
       return;
     }
     const prevItem = firstItemFullyShowing - 1;
-    const x = Math.abs(start) + Math.max((prevItem * itemWidth) - containerWidth, start);
+    const x = Math.abs(start) + Math.max(prevItem * itemWidth - containerWidth, start);
     scrollOuter(x);
   };
 
@@ -203,7 +205,6 @@ export default function dCarousel(el, optionsArg) {
 
     el.classList.remove('init');
   };
-
 
   windowResizeFns.push(windowResizeFunction);
 
@@ -224,11 +225,9 @@ export default function dCarousel(el, optionsArg) {
   // run this function on init to work out the button states
   outerScroll();
 
-
   return {
     destroy,
     getItemsShowing,
-    forceRefresh: windowResizeFunction, // force the calculations to take place again
+    forceRefresh: windowResizeFunction // force the calculations to take place again
   };
-
 }
